35dbe9629c0aa855892830a1f476a3fc88021aca
Valtsu7 <n1kova00@students.oamk.fi>
2024-04-07 19:37:56 +0300

    Pientä muokkausta

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, Pressable, Image, ScrollView } from 'react-native'
+import { Text, View, Pressable, Image, ScrollView, SafeAreaView, StatusBar } from 'react-native'
 import { Container, Row, Col } from 'react-native-flex-grid';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from '../style/style';
@@ -8,13 +8,23 @@ import Header from './Header';
 
 export default Home = (navigation, route) =>{
     return(
-        <View>
-            <Header />
-            <Text style={styles.text} >Welcome to FlavorFriends!</Text>
-            
-            <Text style={styles.text1}> Here is some categories for you! </Text>
-        </View>
 
+       
         
+
+        <SafeAreaView style={styles.container}>
+
+         <ScrollView style={styles.scrollView}>
+         <Header/>
+          <Text style={styles.text}>
+           Welcome to FlavorFriends !
+        </Text>
+
+         <Text style={styles.text1}>
+            Here is some categories for you!
+         </Text>
+    
+      </ScrollView>
+    </SafeAreaView>
     )
 }
