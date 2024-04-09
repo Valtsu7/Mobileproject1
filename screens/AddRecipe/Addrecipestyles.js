dcb3aef21a3f7243059f3cab73d219c8a369d9a1
Valtsu7 <n1kova00@students.oamk.fi>
2024-04-09 15:58:04 +0300

    Addrecipestyles

---
+++ screens/AddRecipe/Addrecipestyles.js
@@ -0,0 +1,39 @@
+import { StyleSheet, StatusBar } from 'react-native';
+
+export default StyleSheet.create({
+ 
+  container: { //
+    flex: 1,
+  },
+
+ 
+  scrollView: {
+    marginHorizontal: 0,
+    maxHeight: 200,
+  },
+
+  button: {
+    marginBottom: 20,
+  },
+ 
+  
+
+  text: { //
+    fontSize: 22, 
+    marginLeft: 40,
+   
+  }, 
+
+  input: { //
+    height: 40,
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 10,
+    paddingHorizontal: 10, 
+    
+  },
+ 
+  
+});
